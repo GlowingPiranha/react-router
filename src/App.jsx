b7250409b24e0c2c26ff821350516f8f6610d461
GlowingPiranha <ikimus01@gmail.com>
2025-09-07 16:34:18 +0200

    [upd] routes+layout-function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import layout from "./components/layout"
+import home from "./pages/home"
+import about from "./pages/about"
+import products from "./pages/products"
+import productDetail from "./pages/productDetail"
 
 
 function App() {
 
-
   return (
+    <Routes>
+
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products">
+          <Route index element={<Products />} />
+          <Route path=":id" element={<ProductDetail />} />
+        </Route>
+        <Route path="*" element={<h1>404 — Pagina non trovata</h1>} />
+      </Route>
+
+    </Routes>
   )
 }
 
---
+++ src/components/layout.jsx
@@ -0,0 +1,28 @@
+import { Outlet, NavLink } from "react-router-dom";
+
+function layout() {
+  return (
+    <div>
+      <nav className="navbar">
+        <NavLink to="/" end className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}>
+          Home
+        </NavLink>
+
+        <NavLink to="/about" end className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}>
+          Chi siamo
+        </NavLink>
+
+        <NavLink to="/products" end className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}>
+          Prodotti
+        </NavLink>
+
+      </nav>
+
+      <main className="container">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default layout
